6caf126ff90e55dc2e5a0dc5aadf16a14aaa7d1c
mwakisaghu <harrisonchambela2016@gmail.com>
2021-10-13 09:38:08 +0300

    This shows how to fetch bootcamps - GET method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,8 +7,9 @@ exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
 
-    res.status(200).json({ success: true, data: bootcamp });
-  } catch (error) {
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    console.log(err);
     res.status(400).json({ success: false });
   }
 };
@@ -16,11 +17,18 @@ exports.getBootcamps = async (req, res, next) => {
 //@desc          Get all bootcamp
 //@route         GET /api/v1/bootcamps/:id
 //@access        Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    sucess: true,
-    data: { msg: `Show bootcamp ${req.params.id}` },
-  });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@desc          Create New bootcamp
