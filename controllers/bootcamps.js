8c63ca77247be61284935ac668899824548198a4
mwakisaghu <harrisonchambela2016@gmail.com>
2021-10-29 12:56:31 +0300

    This shows how to 'Cascade Delete Courses when a bootcamp is deleted'

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -133,7 +133,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 //@route         DELETE /api/v1/bootcamps/:id
 //@access        Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(
@@ -142,6 +142,9 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
       )
     );
   }
+
+  bootcamp.remove();
+
   res.status(200).json({ success: true, data: {} });
 });
 
