053e118650d5815fc2b500e2bcb3fb9687eb875f
mwakisaghu <harrisonchambela2016@gmail.com>
2021-11-03 14:15:01 +0300

    Advanced Results Middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,80 +10,10 @@ const { json } = require('express');
 //@access        Public
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-
-  //making a copy of req.query
-  const reqQuery = { ...req.query };
-
-  //Array of field to exclude
-  const removeFields = ['select', 'sort', 'page', 'limit'];
-
-  //Loop over removeFields & delete them from reqQuery
-  removeFields.forEach((param) => delete reqQuery[param]);
-
-  //create a query string
-  let queryStr = JSON.stringify(reqQuery);
-
-  //create operation (gt, gte, lte etc)
-  queryStr = queryStr.replace(
-    /\b(gt|gte|lt|lte|in)\b/g,
-    (match) => `$${match}`
-  );
-
-  //finding resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
-
-  //Select Fields
-  if (req.query.select) {
-    const fields = req.query.select.split(',').join(' ');
-    query = query.select(fields);
-  }
-
-  //Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  //Paginatio
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  //executing query
-  const bootcamps = await query;
-
-  //Pagination result
-  const pagination = {};
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    pagination,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
-//@desc          Get all bootcamp
+//@desc          Get single bootcamp
 //@route         GET /api/v1/bootcamps/:id
 //@access        Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
