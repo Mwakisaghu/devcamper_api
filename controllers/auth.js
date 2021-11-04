29081d25305cdbac455a39dd5ad3fb397a8ac857
mwakisaghu <harrisonchambela2016@gmail.com>
2021-11-04 13:11:13 +0300

    This shows how to add a 'Sign Get JSON Web Token'

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,5 +15,9 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
-  res.status(200).json({ sucess: true });
+
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ sucess: true, token });
 });
