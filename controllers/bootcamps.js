010dc756dcc51127569573fdac25f0e788fdfcad
mwakisaghu <harrisonchambela2016@gmail.com>
2021-11-11 12:48:25 +0300

    This shows how to add a 'Bootcamp User Relationship'

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -34,6 +34,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@route         POST /api/v1/bootcamps
 //@access        Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  //Add User to req.body
+  req.body.user = req.user.id;
+
+  //Check for published Bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  //If the user is not only an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The User with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({
     success: true,
